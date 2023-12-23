519e75ef693847d8c4f77e2741d161de4d1f0349
Michael Rittmeister <michael@rittmeister.in>
2023-12-23 14:17:42 +0100

    Fix tests

--- client/src/commonTest/kotlin/SearchTest.kt
+++ client/src/commonTest/kotlin/SearchTest.kt
@@ -1,16 +1,15 @@
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
 class SearchTest {
 
     @Test
     fun `search for isrc`() = withClient {
-        val result = search("frx762175060")
+        val result = search("frx762175060", "de")
         println(result)
 
         assertEquals(1, result.size)
         val track= result.first()
-        assertEquals("We Got the Moves", track.title)
+        assertEquals("8ePP9MTcO5w", track.videoId)
     }
 }
