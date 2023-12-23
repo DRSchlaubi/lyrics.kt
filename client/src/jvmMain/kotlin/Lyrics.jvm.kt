05951019ea638682e46ad0f2e846a62dd7fe62ac
Michael Rittmeister <michael@rittmeister.in>
2023-12-23 20:05:09 +0100

    Fix non top search and shrink plugin size

--- client/src/jvmMain/kotlin/Lyrics.jvm.kt
+++ client/src/jvmMain/kotlin/Lyrics.jvm.kt
@@ -1,6 +1,5 @@
 package dev.schlaubi.lyrics
 
 import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
 
-internal actual fun newClient(config: HttpClientConfig<*>.() -> Unit):HttpClient = HttpClient(OkHttp, config)
+internal actual fun newClient(config: HttpClientConfig<*>.() -> Unit): HttpClient = HttpClient(config)
